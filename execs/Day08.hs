67840f87fe8e935949ff67fb450f2b291e5fcd5f
Eric Mertens <emertens@galois.com>
2020-12-08 13:18:52 -0800

    Fiddle with 8

--- execs/Day08.hs
+++ execs/Day08.hs
@@ -13,37 +13,54 @@ module Main (main) where
 
 import           Advent
 import           Control.Applicative
-import qualified Data.Vector.Generic as V
+import           Data.Maybe (fromJust, mapMaybe)
 import qualified Data.Graph.Inductive as G
+
+-- | Programs are expressed as control-flow graphs.
+--
+-- Nodes are instructions in the program.
+--
+-- Nodes are labeled with the accumulator-effect of executing that instruction.
+--
+-- Edges capture control flow between instructions.
+--
+-- Edges are labeled with the /cost/ of taking that edge. It costs @1@ to
+-- take a control path generated from a toggled instruction.
+type Cfg = G.Gr Int Int
+
+------------------------------------------------------------------------
 
 data Opcode = Nop | Acc | Jmp
 
 opcode :: Parser Opcode
 opcode = Nop <$ "nop" <|> Acc <$ "acc" <|> Jmp <$ "jmp"
 
-statement :: Parser (Opcode, Int)
-statement = (,) <$> opcode <* " " <*> number
+instruction :: Parser (Opcode, Int)
+instruction = (,) <$> opcode <* " " <*> number
+
+------------------------------------------------------------------------
 
 main :: IO ()
 main =
-  do inp <- V.fromList <$> getParsedLines 8 statement
-     let g       = pgmGraph inp
-         p1      = G.dfs [0] (G.elfilter (0==) g)
-         Just p2 = G.sp 0 (V.length inp) g
+  do cfg <- pgmToCfg <$> getParsedLines 8 instruction
+     print (pathSum cfg (G.dfs [0] (G.elfilter (0==) cfg)))
+     print (pathSum cfg (fromJust (G.sp 0 (G.noNodes cfg-1) cfg)))
 
-     print (pathSum inp p1)
-     print (pathSum inp p2)
+-- | Sum of node labels along a path in a graph.
+pathSum :: Cfg -> G.Path -> Int
+pathSum inp path = sum (mapMaybe (G.lab inp) path)
 
-pathSum :: Vector (Opcode, Int) -> [Int] -> Int
-pathSum inp path = sum [n | i <- path, Just (Acc, n) <- [inp V.!? i]]
-
-pgmGraph :: Vector (Opcode, Int) -> G.Gr () Int
-pgmGraph pgm =
+pgmToCfg :: [(Opcode, Int)] -> Cfg
+pgmToCfg pgm =
   G.mkGraph
-    [(i, ()) | i <- [0..V.length pgm]]
-    [e | i <- [0..V.length pgm-1], e <- pgmEdge i (pgm V.! i)]
+    (zip [0..] (map accEffect pgm ++ [0]))
+    (concat (zipWith edge [0..] pgm))
 
-pgmEdge :: Int -> (Opcode, Int) -> [G.LEdge Int]
-pgmEdge start (Nop, n) = [(start, start+1, 0), (start, start+n, 1)]
-pgmEdge start (Jmp, n) = [(start, start+1, 1), (start, start+n, 0)]
-pgmEdge start (Acc, _) = [(start, start+1, 0)]
+accEffect :: (Opcode, Int) -> Int
+accEffect (Acc, n) = n
+accEffect _        = 0
+
+edge :: Int -> (Opcode, Int) -> [G.LEdge Int]
+edge i (Nop, n) = [(i, i+1, 0), (i, i+n, 1)]
+edge i (Jmp, n) = [(i, i+n, 0), (i, i+1, 1)]
+edge i (Acc, _) = [(i, i+1, 0)]
