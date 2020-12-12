2aff56a7b9155f89d0cf1bfa057abf82832d3379
Eric Mertens <emertens@galois.com>
2020-12-12 11:09:01 -0800

    Update Day09.hs

--- execs/Day09.hs
+++ execs/Day09.hs
@@ -13,6 +13,10 @@ module Main (main) where
 import           Advent (UVector, getParsedLines, decimal)
 import qualified Data.Vector.Generic as V
 
+-- |
+-- >>> :main
+-- 18272118
+-- 2186361
 main :: IO ()
 main =
   do inp <- V.fromList <$> getParsedLines 9 decimal
@@ -20,23 +24,33 @@ main =
      let target = part1 inp
      print target
 
-     let range = part2 target inp 0 0 0
+     let range = part2 inp 0 target
      print (V.minimum range + V.maximum range)
 
+-- | Returns 'True' when no pair of numbers exists in the vector that
+-- sums up to the given target value.
 search :: Int -> UVector Int -> Bool
 search target haystack = null
   [() | i <- [0   .. V.length haystack - 2]
       , j <- [i+1 .. V.length haystack - 1]
       , haystack V.! i + haystack V.! j == target]
 
+-- | Find a number in the vector that is /not/ the sum of any pair of
+-- numbers in the 25 elements preceeding it.
 part1 :: UVector Int -> Int
 part1 v
   | search (v V.! 25) (V.take 25 v) = v V.! 25
   | otherwise = part1 (V.tail v)
 
-part2 :: Int -> UVector Int -> Int -> Int -> Int -> UVector Int
-part2 target v acc lo hi =
-  case compare acc target of
-    EQ | hi-lo > 1 -> V.slice lo (hi-lo) v
-    LT             -> part2 target v (acc + v V.! hi) lo (hi+1)
-    _              -> part2 target v (acc - v V.! lo) (lo+1) hi
+-- | Find a contiguous subsequence of the given vector that sums to
+-- the given target.
+part2 ::
+  UVector Int {- ^ remaining elements                -} ->
+  Int         {- ^ leading elements used             -} ->
+  Int         {- ^ remaining target value            -} ->
+  UVector Int {- ^ subsequence matching target value -}
+part2 v n acc =
+  case compare acc 0 of
+    EQ | n > 1 -> V.take n v                              -- done
+    GT         -> part2 v          (n+1) (acc - v V.! n)  -- use another element
+    _          -> part2 (V.tail v) (n-1) (acc + V.head v) -- stop using an element
