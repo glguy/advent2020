fbb9e0f1a2fdd8ac3ad22ecfba022d0c6f89f057
Eric Mertens <emertens@gmail.com>
2020-12-13 16:50:46 -0800

    Update Day13.hs

--- execs/Day13.hs
+++ execs/Day13.hs
@@ -8,15 +8,16 @@ Maintainer  : emertens@gmail.com
 
 <https://adventofcode.com/2020/day/13>
 
+While the general case of part2 can be solved efficiently with
+the chinese remainder theorem, this local implementation is
+much more straight-forward.
+
 -}
 module Main (main) where
 
 import Advent
-import Advent.Chinese
-import Data.Ord            (comparing)
-import Data.List           (minimumBy)
+import Data.List           (foldl1')
 import Control.Applicative ((<|>))
-import Data.Maybe          (fromJust)
 
 busId :: Parser (Maybe Integer)
 busId = Nothing <$ "x" <|> Just <$> decimal
@@ -32,8 +33,18 @@ main :: IO ()
 main =
   do (t,rawBusses) <- getParsedInput 13 format
      let busses = [(i,b) | (i, Just b) <- zip [0..] rawBusses]
-     print $ part1 $ minimumBy (comparing residue) [toMod (-t) b | (_,b) <- busses]
-     print $ fromJust $ chinese                    [toMod (-u) b | (u,b) <- busses]
+     print (part1 t (map snd busses))
+     print (part2 busses)
 
-part1 :: Mod -> Integer
-part1 x = residue x * modulus x
+toMod :: Integer -> Integer -> (Integer,Integer)
+toMod x y = (x`mod`y, y)
+
+part1 :: Integer -> [Integer] -> Integer
+part1 t busses = uncurry (*) (minimum [toMod (-t) b | b <- busses])
+
+part2 :: [(Integer,Integer)] -> Integer
+part2 busses = fst (foldl1' combine [toMod (-u) b | (u,b) <- busses])
+  where
+    combine (n,m) (a,b)
+      | n `rem` b == a = (n, lcm m b)
+      | otherwise      = combine (n+m, m) (a,b)
