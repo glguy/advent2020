2c24fd4554dd8d7ba7a4a3e9549f32ce7f3ba21b
Eric Mertens <emertens@galois.com>
2020-12-15 11:00:47 -0800

    Add benchmarking module

---
+++ common/Advent/Bench.hs
@@ -0,0 +1,29 @@
+{-# Language BlockArguments #-}
+{-|
+Module      : Advent.Bench
+Description : Helper for running benchmarks
+Copyright   : (c) Eric Mertens, 2020
+License     : ISC
+Maintainer  : emertens@gmail.com
+
+-}
+module Advent.Bench (NFData(rnf), benchMain) where
+
+import Control.DeepSeq
+import Criterion.Main
+import System.Environment
+
+benchMain ::
+  NFData a =>
+  IO a ->
+  [(String, a -> b)] ->
+  IO ()
+benchMain getInput impls =
+  do args <- getArgs
+     let args' = drop 1 (dropWhile ("--" /=) args)
+     withArgs args' $
+       defaultMain
+         [ env getInput \inp ->
+           bgroup "Implementations"
+           [bench name (whnf impl inp) | (name, impl) <- impls]
+         ]
