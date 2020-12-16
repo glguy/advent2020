35e265bc50b0fbe53c0b4778b7bdedfc40654b1d
Eric Mertens <emertens@gmail.com>
2020-12-16 08:47:00 -0800

    Update Day16.hs

--- execs/Day16.hs
+++ execs/Day16.hs
@@ -13,8 +13,7 @@ module Main (main) where
 
 import Advent
 import Control.Applicative (some)
-import Data.List (delete, isPrefixOf, sortBy, transpose)
-import Data.Ord (comparing)
+import Data.List (delete, isPrefixOf, sortOn, transpose)
 
 data Range = Range Int Int deriving Show
 
@@ -59,23 +58,14 @@ main =
               : [xs | xs <- nearbyTickets, all (\x -> any (`match` x) fields) xs]
 
      let possible
-           = sortBy (comparing (length . snd))
+           = sortOn (length . snd)
            $ zip yourTicket
                  [ [fieldName field | field <- fields, all (match field) col]
                  | col <- transpose good]
 
-     print (product [i | (i, name) <- backprop possible
+     print (product [i | (i, name) <- head (search possible)
                        , "departure" `isPrefixOf` name])
 
-backprop :: Eq b => [(a, [b])] -> [(a,b)]
-backprop xs = löb [pick a bs | (a,bs) <- xs]
-
-pick :: Eq b => a -> [b] -> [(a,b)] -> (a,b)
-pick a [b] _        = (a,b)
-pick a bs ((_,y):z) = pick a (delete y bs) z
-pick _ _  _         = error "backprop failed"
-
-{-
 search :: Eq b => [(a, [b])] -> [[(a,b)]]
 search [] = [[]]
 search ((i,names):xs) =
@@ -83,4 +73,3 @@ search ((i,names):xs) =
   | name <- names
   , rest <- search [(a, delete name b) | (a,b) <- xs]
   ]
--}
