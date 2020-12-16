c186bb02c684ea2029def7f4e0752bddb9152358
Eric Mertens <emertens@gmail.com>
2020-12-15 22:54:22 -0800

    Update 16

--- execs/Day16.hs
+++ execs/Day16.hs
@@ -1,4 +1,4 @@
-{-# Language OverloadedStrings #-}
+{-# Language ImportQualifiedPost, OverloadedStrings #-}
 {-|
 Module      : Main
 Description : Day 16 solution
@@ -11,15 +11,20 @@ Maintainer  : emertens@gmail.com
 -}
 module Main (main) where
 
-import           Advent
-import           Control.Applicative
-import           Data.List
-import           Data.Char
-import           Data.Ord
+import Advent
+import Control.Applicative
+import Data.Set (Set)
+import Data.Set qualified as Set
+import Data.List
+import Data.Char
+import Data.Ord
 
 data Range = Range Int Int deriving Show
 
 data Field = Field String Range Range deriving Show
+
+fieldName :: Field -> String
+fieldName (Field n _ _) = n
 
 pRange :: Parser Range
 pRange = Range <$> decimal <* "-" <*> decimal
@@ -46,14 +51,16 @@ main =
      let good = [ xs | xs <- nearbyTickets
                      , all (\x -> any (\field -> match field x) fields) xs]
 
-     let columns
-           = sortBy (comparing (\(_,x) -> count (\field -> all (match field) x) fields))
-           $ zip [0::Int ..] (transpose good)
+     let possible
+           = sortBy (comparing (length . snd))
+           $ zip [0::Int ..]
+                 [ [fieldName field | field <- fields, all (match field) col]
+                 | col <- transpose good ]
 
-     let mapping = head (search fields columns)
+     let mapping = head (search Set.empty possible)
 
      print (product [yourTicket !! i
-                    | (i, Field name _ _) <- mapping, "departure" `isPrefixOf` name])
+                    | (i, name) <- mapping, "departure" `isPrefixOf` name])
 
 match1 :: Range -> Int -> Bool
 match1 (Range lo hi) x = lo <= x && x <= hi
@@ -61,12 +68,11 @@ match1 (Range lo hi) x = lo <= x && x <= hi
 match :: Field -> Int -> Bool
 match (Field _ x y) z = match1 x z || match1 y z
 
-search :: [Field] -> [(Int, [Int])] -> [[(Int,Field)]]
-search []     []         = [[]]
-search fields ((i,x):xs) =
-  [ (i, field):rest
-  | (field, fields') <- pickOne fields
-  , all (match field) x
-  , rest <- search fields' xs
+search :: Ord a => Set a -> [(Int, [a])] -> [[(Int,a)]]
+search _     []         = [[]]
+search seen ((i,names):xs) =
+  [ (i, name):rest
+  | name <- names
+  , Set.notMember name seen
+  , rest <- search (Set.insert name seen) xs
   ]
-search _ _ = []
