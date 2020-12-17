8b5d2329f19d0603fdf30fe571e9df0a302cb04d
Eric Mertens <emertens@galois.com>
2020-12-17 15:04:26 -0800

    Update Day17.hs

--- execs/Day17.hs
+++ execs/Day17.hs
@@ -18,15 +18,12 @@ import Data.Set qualified as Set
 import Data.Map (Map)
 import Data.Map.Strict qualified as Map
 
--- | N-dimensional coordinates
-type C = [Int]
-
 -- | Find the coordinates of live cells.
 --
 -- >>> parse [".#.", "..#", "###"]
 -- [[1,0],[2,1],[0,2],[1,2],[2,2]]
-parse :: [String] -> [C]
-parse input = [[x,y] | (y,line) <- zip [0..] input, (x,'#') <- zip [0..] line]
+parse :: [String] -> [(Int,Int)]
+parse input = [(x,y) | (y,line) <- zip [0..] input, (x,'#') <- zip [0..] line]
 
 -- |
 -- >>> :main
@@ -35,37 +32,56 @@ parse input = [[x,y] | (y,line) <- zip [0..] input, (x,'#') <- zip [0..] line]
 main :: IO ()
 main =
   do inp <- parse <$> getInputLines 17
-     print (run 3 inp)
-     print (run 4 inp)
+     print (run (map toC3 inp))
+     print (run (map toC4 inp))
 
 run ::
-  Int     {- ^ dimension                      -} ->
-  [[Int]] {- ^ input x,y coordinates          -} ->
-  Int     {- ^ live cells after 6 generations -}
-run d
-  = Set.size
-  . times 6 (step (neighborhood d))
-  . Set.mapMonotonic (replicate (d-2) 0 ++)
-  . Set.fromList
-
--- | Compute distance 1 neighborhood around the origin for a given dimension.
---
--- >>> neighborhood 2
--- fromList [([-1,-1],1),([-1,0],1),([-1,1],1),([0,-1],1),([0,1],1),([1,-1],1),([1,0],1),([1,1],1)]
-neighborhood :: Int -> Map C Int
-neighborhood d = Map.fromList [(c,1) | c <- tail (replicateM d [0,-1,1])]
-
--- | Compute the next generation from the previous generation
-step :: Map C Int -> Set C -> Set C
-step d world
-  = Map.keysSet
-  $ Map.filterWithKey (rule world)
-  $ Map.unionsWith (+) [Map.mapKeysMonotonic (zipWith (+) s) d | s <- Set.toList world ]
+  Coord a =>
+  [a] {- ^ input coordinates              -} ->
+  Int {- ^ live cells after 6 generations -}
+run = Set.size . times 6 step . Set.fromList
 
 -- | Determine if a cell should be alive in the next generation.
 rule ::
-  Set C {- ^ previous generation      -} ->
-  C     {- ^ coordinate               -} ->
+  Ord a =>
+  Set a {- ^ previous generation      -} ->
+  a     {- ^ coordinate               -} ->
   Int   {- ^ live neighbor count      -} ->
   Bool  {- ^ alive in next generation -}
 rule world c n = n == 3 || n == 2 && Set.member c world
+
+-- | Compute the next generation from the previous generation
+step :: Coord a => Set a -> Set a
+step world
+  = Map.keysSet
+  $ Map.filterWithKey (rule world)
+  $ Map.unionsWith (+)
+  $ map neighborCount
+  $ Set.toList world
+
+-- Unpacked, custom tuples improves my runtime by a factor of 3 over the list of Int version
+
+data C3 = C3 !Int !Int !Int      deriving (Eq, Ord)
+data C4 = C4 !Int !Int !Int !Int deriving (Eq, Ord)
+
+toC3 :: (Int,Int) -> C3
+toC3 (x,y) = C3 x y 0
+
+toC4 :: (Int,Int) -> C4
+toC4 (x,y) = C4 x y 0 0
+
+-- | Compute a Map with @1@ stored at each neighboring coordinate
+neighborCount :: Coord a => a -> Map a Int
+neighborCount c = Map.mapKeysMonotonic (add c) neighborhood
+
+class Ord a => Coord a where
+  add          :: a -> a -> a
+  neighborhood :: Map a Int -- Defined as a CAF so that it is only computed once
+
+instance Coord C3 where
+  add (C3 a b c) (C3 x y z) = C3 (a+x) (b+y) (c+z)
+  neighborhood = Map.fromList [(C3 x y z,1) | [x,y,z] <- tail (replicateM 3 [0,-1,1])]
+
+instance Coord C4 where
+  add (C4 a b c d) (C4 x y z w) = C4 (a+x) (b+y) (c+z) (d+w)
+  neighborhood = Map.fromList [(C4 x y z w,1) | [x,y,z,w] <- tail (replicateM 4 [0,-1,1])]
