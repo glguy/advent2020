86d9de261b932f48788135d7fe56165affe5c832
Eric Mertens <emertens@galois.com>
2020-12-18 10:47:39 -0800

    Update Main.hs

--- alexhappy-18/Main.hs
+++ alexhappy-18/Main.hs
@@ -16,12 +16,12 @@ import Parser
 
 main :: IO ()
 main =
-  do inp <- lines <$> getRawInput 18
+  do inp <- getInputLines 18
      let tokens = map alexScanTokens inp
      print (sum (map (eval . expr1) tokens))
      print (sum (map (eval . expr2) tokens))
 
 eval :: Expr -> Integer
 eval (Integer i) = i
-eval (Add x y) = eval x + eval y
-eval (Mul x y) = eval x * eval y
+eval (Add x y)   = eval x + eval y
+eval (Mul x y)   = eval x * eval y
