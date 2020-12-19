dee6c3917d149b0dd3ef1594216a9a5bd0a1f631
Eric Mertens <emertens@galois.com>
2020-12-18 17:14:42 -0800

    Update Day18.hs

--- execs/Day18.hs
+++ execs/Day18.hs
@@ -1,4 +1,4 @@
-{-# Language BlockArguments, OverloadedStrings #-}
+{-# Language BlockArguments, ViewPatterns, OverloadedStrings #-}
 {-|
 Module      : Main
 Description : Day 18 solution
@@ -13,7 +13,7 @@ module Main (main) where
 
 import Advent
 import Control.Applicative
-import Data.Char (digitToInt, isDigit)
+import Data.Char (isDigit)
 
 data Expr = Add Expr Expr | Mul Expr Expr | Lit Integer
   deriving Show
@@ -33,8 +33,8 @@ main =
 
      -- redo the problem with shunting-yard
      inp <- getInputLines 18
-     print (sum (map (eval . shunt p1prec [] []) inp))
-     print (sum (map (eval . shunt p2prec [] []) inp))
+     print (sum (map (eval . shuntParser p1prec) inp))
+     print (sum (map (eval . shuntParser p2prec) inp))
 
 ------------------------------------------------------------------------
 -- parser combinators solution -----------------------------------------
@@ -67,36 +67,49 @@ expr2 = chainl1 (chainl1 (aexpr expr2) add) mul
 -- Manual operator parsing ---------------------------------------------
 ------------------------------------------------------------------------
 
+-- | Parser mode.
+data Mode
+  = N -- | Expecting a number or @(@
+  | O -- | Expecting an operator
+  deriving Show
+
+shuntParser :: (Char -> Int) -> String -> Expr
+shuntParser p = shunt p N [] []
+
 shunt ::
   (Char -> Int) {- ^ operator precedence -} ->
+  Mode          {- ^ parser mode         -} ->
   [Expr]        {- ^ output stack        -} ->
   [Char]        {- ^ operator stack      -} ->
   String        {- ^ input string        -} ->
   Expr          {- ^ result              -}
 -- parsing complete ----------------------------------------------------
-shunt _ [v] [] ""                           = v
+shunt _ O [v]  []  ""                         = v
 -- skip whitespace -----------------------------------------------------
-shunt p vals ops (' ':str)                  = shunt p vals ops str
+shunt p m vals ops (' ':str)                  = shunt p m vals ops str
 -- manage parentheses matching -----------------------------------------
-shunt p vals ops       ('(':str)            = shunt p vals ('(':ops) str
-shunt p vals ('(':ops) (')':str)            = shunt p vals ops       str
+shunt p O vals ('(':ops) (')':str)            = shunt p O vals ops       str
+shunt p N vals ops       ('(':str)            = shunt p N vals ('(':ops) str
 -- push onto operator stack --------------------------------------------
-shunt p vals ops ('+':str) | push p ops '+' = shunt p vals ('+':ops) str
-shunt p vals ops ('*':str) | push p ops '*' = shunt p vals ('*':ops) str
--- push onto value stack -----------------------------------------------
-shunt p vals ops (d:str) | isDigit d        = shunt p (evalDigit d:vals) ops str
+shunt p O vals ops ('+':str) | push p ops '+' = shunt p N vals ('+':ops) str
+shunt p O vals ops ('*':str) | push p ops '*' = shunt p N vals ('*':ops) str
+-- push literals onto output stack -------------------------------------
+shunt p N vals ops (lit -> Just (n,str))      = shunt p O (Lit n:vals) ops str
 -- reduction cases -----------------------------------------------------
-shunt p (y:x:vs) ('+':ops) str              = shunt p (Add x y:vs) ops str
-shunt p (y:x:vs) ('*':ops) str              = shunt p (Mul x y:vs) ops str
+shunt p O (y:x:vs) ('+':ops) str              = shunt p O (Add x y:vs) ops str
+shunt p O (y:x:vs) ('*':ops) str              = shunt p O (Mul x y:vs) ops str
 -- error case ----------------------------------------------------------
-shunt _ vals ops str = error ("Parse error: " ++ show (vals, ops, str))
+shunt _ m vals ops str = error ("Parse error: " ++ show (m, vals, ops, str))
+
+lit :: String -> Maybe (Integer, String)
+lit str =
+  case span isDigit str of
+    ([],_)    -> Nothing
+    (ds,rest) -> Just (read ds, rest)
 
 push :: (Char -> Int) -> [Char] -> Char -> Bool
 push _ []    _ = True
 push p (o:_) c = p c > p o
-
-evalDigit :: Char -> Expr
-evalDigit = Lit . fromIntegral . digitToInt
 
 p1prec :: Char -> Int
 p1prec '(' = 0
