6c65cd76bd701cdebed24178862fb577eb0a387f
Eric Mertens <emertens@gmail.com>
2020-12-19 08:29:39 -0800

    Update Day19.hs

--- execs/Day19.hs
+++ execs/Day19.hs
@@ -18,23 +18,48 @@ import Data.IntMap (IntMap)
 import Data.IntMap qualified as IntMap
 import Text.ParserCombinators.ReadP qualified as R
 
+-- | Rules either match a literal string, or match a sum
+-- of product of sub-rules.
 type Rule = Either String [[Int]]
 
+-- | Lexeme parser that trims trailing spaces
 l :: Parser a -> Parser a
 l p = p <* spaces
 
+-- | List of rules followed by examples
 format :: Parser ([(Int,Rule)], [String])
 format =
-  (,) <$> endBy rule "\n" <* "\n"
+  (,) <$> endBy rule "\n"
+      <*  "\n"
       <*> endBy (many letterChar) "\n"
 
+-- | Number-prefixed 'rhs'
 rule :: Parser (Int, Rule)
 rule = (,) <$> decimal <* l ":" <*> rhs
 
+-- | Matches 'literal' or 'alts'
 rhs :: Parser Rule
-rhs = Left  <$ "\"" <*> many letterChar <* "\"" <|>
-      Right <$> many (l decimal) `sepBy` l "|"
+rhs = Left <$> literal <|> Right <$> alts
 
+-- | Matches @|@ separated 'seqs': @1 2 | 3 4@
+alts :: Parser [[Int]]
+alts = seqs `sepBy` l "|"
+
+-- | Matches lists of integers: @1 2 3 4@
+seqs :: Parser [Int]
+seqs = many (l decimal)
+
+-- | Matches string literals: @"example"@
+literal :: Parser String
+literal = "\"" *> many letterChar <* "\""
+
+------------------------------------------------------------------------
+
+-- | Print answers.
+--
+-- >>> :main
+-- 180
+-- 323
 main :: IO ()
 main =
   do (rs,ws) <- getParsedInput 19 format
@@ -47,12 +72,13 @@ main =
      print (run rules1 ws)
      print (run rules2 ws)
 
-run :: IntMap Rule -> [String] -> Int
+run ::
+  IntMap Rule {- ^ parse rules                      -} ->
+  [String]    {- ^ input strings                    -} ->
+  Int         {- ^ number of matching input strings -}
 run rules ws = count (not . null . R.readP_to_S parser) ws
   where
-    parser  = parsers IntMap.! 0 *> R.eof
+    parser = löb (ruleParser <$> rules) IntMap.! 0 *> R.eof
 
-    parsers = toParser <$> rules
-
-    toParser (Left s)    = () <$ R.string s
-    toParser (Right xss) = asum [traverse_ (parsers IntMap.!) xs | xs <- xss]
+    ruleParser (Left  s  ) _   = () <$ R.string s
+    ruleParser (Right xss) sub = asum [traverse_ (sub IntMap.!) xs | xs <- xss]
