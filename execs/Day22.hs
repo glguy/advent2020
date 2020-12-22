5058b81755f5da071f9a668b1b8b9901fda6f223
Eric Mertens <emertens@galois.com>
2020-12-22 10:44:18 -0800

    Update Day22.hs

--- execs/Day22.hs
+++ execs/Day22.hs
@@ -14,7 +14,7 @@ module Main (main) where
 import Advent
 import Advent.Format (format)
 import Data.Foldable
-import Data.Sequence (Seq)
+import Data.Sequence (Seq(..))
 import Data.Sequence qualified as Seq
 import Data.Set (Set)
 import Data.Set qualified as Set
@@ -32,38 +32,56 @@ main =
      print (score (play1 p1 p2))
      print (score (snd (play2 Set.empty p1 p2)))
 
+-- | Game deck, draw from left side
 type Deck = Seq Int
 
 score :: Deck -> Int
 score = sum . zipWith (*) [1..] . reverse . toList
 
+-- | representation of a game state used to find cycles
+type Rep = UVector Int
+
+characterize :: Deck -> Deck -> Rep
+characterize xs ys = V.fromList (toList xs ++ (-1) : toList ys)
+
+------------------------------------------------------------------------
+
+-- | Play the game according to part 1 rules and return the winning deck
 play1 :: Deck -> Deck -> Deck
-play1 Seq.Empty xs = xs
-play1 xs Seq.Empty = xs
-play1 (x Seq.:<| xs) (y Seq.:<| ys)
-  | x > y     = play1 (give xs x y) ys
-  | otherwise = play1 xs (give ys y x)
+play1 Empty xs = xs
+play1 xs Empty = xs
+play1 (x :<| xs) (y :<| ys)
+  | x > y     = play1 (xs :|> x :|> y) ys
+  | otherwise = play1 xs (ys :|> y :|> x)
 
-play2 :: Set (UVector Int) -> Deck -> Deck -> (Bool, Deck)
-play2 _ Seq.Empty xs = (False, xs)
-play2 _ xs Seq.Empty = (True, xs)
-play2 seen xxs@(x Seq.:<| xs) yys@(y Seq.:<| ys)
+------------------------------------------------------------------------
 
-  -- p1 wins loops
+-- | Play the game according to part 2 rules and report if player 1 won
+-- and the winning deck. Takes a set of previously seen game states to
+-- eliminate loops.
+play2 :: Set Rep -> Deck -> Deck -> (Bool, Deck)
+play2 _ Empty xs = (False, xs)
+play2 _ xs Empty = (True, xs)
+play2 seen xxs@(x :<| xs) yys@(y :<| ys)
+
+  -- P1 wins loops
   | Set.member here seen = (True, xxs)
 
   -- recursive game
   | x <= Seq.length xs, y <= Seq.length ys
   , let x' = Seq.take x xs
   , let y' = Seq.take y ys
-  , let x1 = maximum x' -- best p1 card
-  , let y1 = maximum y' -- best p2 card
+  , let x1 = maximum x' -- best P1 card
+  , let y1 = maximum y' -- best P2 card
 
     -- if P1 has the high card that can't be lost to a
     -- recursive game then he will always eventually win:
     -- He'll never lose that card and wins in the case of
-    -- a loop
-  = if x1 > y1 && x1 >= (x+y) || fst (play2 Set.empty x' y')
+    -- a loop. The highest card is always at least as large
+    -- as the number of cards in the game because all cards
+    -- are unique and start at 1, therefore it can never
+    -- trigger a recursive game.
+  = if x1 > y1 || fst (play2 Set.empty x' y')
       then p1win
       else p2win
 
@@ -74,11 +92,6 @@ play2 seen xxs@(x Seq.:<| xs) yys@(y Seq.:<| ys)
   where
     here  = characterize xxs yys
     seen1 = Set.insert here seen
-    p1win = play2 seen1 (give xs x y) ys
-    p2win = play2 seen1 xs (give ys y x)
+    p1win = play2 seen1 (xs :|> x :|> y) ys
+    p2win = play2 seen1 xs (ys :|> y :|> x)
 
-characterize :: Deck -> Deck -> UVector Int
-characterize xs ys = V.fromList (toList xs ++ [-1] ++ toList ys)
-
-give :: Deck -> Int -> Int -> Deck
-give a b c = a Seq.|> b Seq.|> c
