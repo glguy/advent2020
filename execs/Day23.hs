93c593115e4e4a475cf1da058152d48b6cdc56fb
Eric Mertens <emertens@galois.com>
2020-12-23 00:02:12 -0800

    Update Day23.hs

--- execs/Day23.hs
+++ execs/Day23.hs
@@ -12,69 +12,86 @@ Maintainer  : emertens@gmail.com
 module Main (main) where
 
 import Advent.Format (format)
-import Data.Char
-import Data.Foldable
-import Data.List
+import Data.Char (digitToInt)
+import Data.Foldable (for_)
+import Data.List ((\\))
 import Data.Array.IO qualified as A
 
-newRing :: Int -> [Int] -> IO (A.IOUArray Int Int)
+-- | The array maps cup numbers (indexes) to the next cup
+-- in the sequence (elements).
+type Ring = A.IOUArray Int Int
+
+newRing :: Int -> [Int] -> IO Ring
 newRing n order =
   do a <- A.newArray_ (1,n)
-     for_ (zip order (tail (cycle order))) $ \(x,y) ->
+     for_ (zip order (tail order)) $ \(x,y) ->
         A.writeArray a x y
+     A.writeArray a (last order) (head order)
      pure a
 
-readRing :: A.IOUArray Int Int -> Int -> Int -> IO [Int]
-readRing _ _ 0 = pure []
-readRing a i n =
-  do i' <- A.readArray a i
-     rest <- readRing a i' (n-1)
-     pure (i : rest)
+readRing :: Ring -> Int -> Int -> IO [Int]
+readRing a n i
+  | n <= 0 = pure []
+  | otherwise =
+    do i'   <- A.readArray a i
+       rest <- readRing a (n-1) i'
+       pure (i : rest)
 
-steps :: A.IOUArray Int Int -> Int -> Int -> Int -> IO ()
-steps _ _ _ 0 = return ()
-steps a n cur i =
-  do cur' <- step a cur
-     steps a n cur' (i-1)
+chain :: Int -> (a -> IO a) -> a -> IO ()
+chain n f x
+  | n <= 0    = pure ()
+  | otherwise = chain (n-1) f =<< f x
 
+-- |
+-- :main
+-- 47382659
+-- 42271866720
 main :: IO ()
 main =
   do inp <- map digitToInt <$> [format|23 %c*%n|]
      p1 inp
      p2 inp
 
+step :: Ring -> Int -> IO Int
+step a cur =
+  do -- extract a group of three cups
+     g1 <- A.readArray a cur
+     g2 <- A.readArray a g1
+     g3 <- A.readArray a g2
+
+     -- find next cup and link current one to it
+     nx <- A.readArray a g3
+     A.writeArray a cur nx
+
+     -- find the new destination label
+     (lo,hi) <- A.getBounds a
+     let gs = [g1,g2,g3]
+         searchOrder = [cur-1, cur-2 .. lo] ++ [hi, hi-1 .. ]
+         dest:_ = searchOrder \\ gs
+
+     -- splice the group back in at dest
+     A.writeArray a g3 =<< A.readArray a dest
+     A.writeArray a dest g1
+
+     pure nx
+
 p1 :: [Int] -> IO ()
 p1 inp =
   do ring <- newRing (length inp) inp
-     steps ring (length inp) (head inp) 100
-     putStrLn . concatMap show . take 9 . tail =<< readRing ring 1 9
+     chain 100 (step ring) (head inp)
+
+     xs <- readRing ring (length inp) 1
+     putStrLn (concatMap show (tail xs))
 
 p2 :: [Int] -> IO ()
 p2 inp =
   do let sz   =  1_000_000
          iter = 10_000_000
-     let inp' = take sz (inp ++ [maximum inp+1 ..])
+         inp' = take sz (inp ++ [maximum inp+1 ..])
+
      ring <- newRing sz inp'
-     steps ring sz (head inp') iter
-     print . product =<< readRing ring 1 3
+     chain iter (step ring) (head inp')
 
-step :: A.IOUArray Int Int -> Int -> IO Int
-step v cur =
-  do -- extract a group of three nodes
-     g1 <- A.readArray v cur
-     g2 <- A.readArray v g1
-     g3 <- A.readArray v g2
-     g4 <- A.readArray v g3
-     A.writeArray v cur g4
-
-     (lo,hi) <- A.getBounds v
-     -- find the new destination label
-     let gs = [cur,g1,g2,g3]
-         searchOrder = [cur-1, cur-2 .. lo] ++ [hi, hi-1 .. ]
-         dest:_ = searchOrder \\ gs
-
-     -- splice the group in
-     A.writeArray v g3 =<< A.readArray v dest
-     A.writeArray v dest g1
-
-     pure g4
+     x <- A.readArray ring 1
+     y <- A.readArray ring x
+     print (x*y)
