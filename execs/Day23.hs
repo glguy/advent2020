eebd63afa8935f55800410526c9ec913b15781d6
Eric Mertens <emertens@galois.com>
2020-12-23 00:08:39 -0800

    Update Day23.hs

--- execs/Day23.hs
+++ execs/Day23.hs
@@ -1,4 +1,4 @@
-{-# Language NumericUnderscores, BlockArguments, ImportQualifiedPost, OverloadedStrings, QuasiQuotes #-}
+{-# Language BlockArguments, ImportQualifiedPost, NumericUnderscores, QuasiQuotes #-}
 {-|
 Module      : Main
 Description : Day 23 solution
@@ -15,25 +15,25 @@ import Advent.Format (format)
 import Data.Char (digitToInt)
 import Data.Foldable (for_)
 import Data.List ((\\))
-import Data.Array.IO qualified as A
+import Data.Array.IO (IOUArray, getBounds, newArray_, readArray, writeArray)
 
 -- | The array maps cup numbers (indexes) to the next cup
 -- in the sequence (elements).
-type Ring = A.IOUArray Int Int
+type Ring = IOUArray Int Int
 
 newRing :: Int -> [Int] -> IO Ring
 newRing n order =
-  do a <- A.newArray_ (1,n)
-     for_ (zip order (tail order)) $ \(x,y) ->
-        A.writeArray a x y
-     A.writeArray a (last order) (head order)
+  do a <- newArray_ (1,n)
+     for_ (zip order (tail order)) \(x,y) ->
+        writeArray a x y
+     writeArray a (last order) (head order)
      pure a
 
 readRing :: Ring -> Int -> Int -> IO [Int]
 readRing a n i
   | n <= 0 = pure []
   | otherwise =
-    do i'   <- A.readArray a i
+    do i'   <- readArray a i
        rest <- readRing a (n-1) i'
        pure (i : rest)
 
@@ -55,23 +55,23 @@ main =
 step :: Ring -> Int -> IO Int
 step a cur =
   do -- extract a group of three cups
-     g1 <- A.readArray a cur
-     g2 <- A.readArray a g1
-     g3 <- A.readArray a g2
+     g1 <- readArray a cur
+     g2 <- readArray a g1
+     g3 <- readArray a g2
 
      -- find next cup and link current one to it
-     nx <- A.readArray a g3
-     A.writeArray a cur nx
+     nx <- readArray a g3
+     writeArray a cur nx
 
      -- find the new destination label
-     (lo,hi) <- A.getBounds a
+     (lo,hi) <- getBounds a
      let gs = [g1,g2,g3]
          searchOrder = [cur-1, cur-2 .. lo] ++ [hi, hi-1 .. ]
          dest:_ = searchOrder \\ gs
 
      -- splice the group back in at dest
-     A.writeArray a g3 =<< A.readArray a dest
-     A.writeArray a dest g1
+     writeArray a g3 =<< readArray a dest
+     writeArray a dest g1
 
      pure nx
 
@@ -92,6 +92,6 @@ p2 inp =
      ring <- newRing sz inp'
      chain iter (step ring) (head inp')
 
-     x <- A.readArray ring 1
-     y <- A.readArray ring x
+     x <- readArray ring 1
+     y <- readArray ring x
      print (x*y)
