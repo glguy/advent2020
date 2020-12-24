26706fa749af05d9664811ad15c163d8bd4adba7
Eric Mertens <emertens@gmail.com>
2020-12-24 13:30:46 -0800

    Use array on day 3

--- execs/Day03.hs
+++ execs/Day03.hs
@@ -1,4 +1,4 @@
-{-# Language ImportQualifiedPost #-}
+{-# Language ImportQualifiedPost, ParallelListComp #-}
 {-|
 Module      : Main
 Description : Day 3 solution
@@ -13,8 +13,9 @@ Sledding down a slope counting trees.
 -}
 module Main where
 
-import Advent
-import Data.Vector.Generic qualified as V
+import Advent (count, getInputArray)
+import Advent.Coord (Coord(C))
+import Data.Array.Unboxed qualified as A
 
 -- |
 -- >>> :main
@@ -22,7 +23,7 @@ import Data.Vector.Generic qualified as V
 -- 2832009600
 main :: IO ()
 main =
-  do inp <- getInputVector 3
+  do inp <- getInputArray 3
      print $ solve 3 1 inp
      print $ solve 1 1 inp
            * solve 3 1 inp
@@ -30,11 +31,12 @@ main =
            * solve 7 1 inp
            * solve 1 2 inp
 
-solve :: Int -> Int -> Vector (UVector Char) -> Int
-solve dx dy vs
-  = count (\(x,y) -> '#' == vs V.! y !% x)
-  $ zip [0, dx ..] [0, dy .. V.length vs - 1]
-
--- | Modular indexing. Indexes overflow back to the beginning.
-(!%) :: V.Vector v a => v a -> Int -> a
-v !% i = v V.! (i `mod` V.length v)
+solve :: Int -> Int -> A.UArray Coord Char -> Int
+solve dx dy grid
+  = count (\c -> '#' == grid A.! c)
+    [ C y (xlo + xoff `rem` width)
+    | xoff <- [0, dx ..]
+    | y    <- [ylo, ylo+dy .. yhi]]
+  where
+    width = xhi - xlo + 1
+    (C ylo xlo, C yhi xhi) = A.bounds grid
