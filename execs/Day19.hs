ec5b7e3e3ea6d71b7dc55617ec2416dae148c2e3
Eric Mertens <emertens@gmail.com>
2020-12-24 13:49:57 -0800

    Update Day19.hs

--- execs/Day19.hs
+++ execs/Day19.hs
@@ -11,16 +11,16 @@ Maintainer  : emertens@gmail.com
 -}
 module Main (main) where
 
-import Advent
+import Advent (count, löb)
 import Advent.Format (format)
 import Data.Foldable (asum, traverse_)
 import Data.IntMap (IntMap)
 import Data.IntMap qualified as IntMap
-import Text.ParserCombinators.ReadP qualified as R
+import Text.ParserCombinators.ReadP (ReadP, eof, readP_to_S, string)
 
 -- | Rules either match a literal string, or match a sum
 -- of product of sub-rules.
-type Rule = Either Char [[Int]]
+type Rule = Either String [[Int]]
 
 ------------------------------------------------------------------------
 
@@ -31,17 +31,11 @@ type Rule = Either Char [[Int]]
 -- 323
 main :: IO ()
 main =
-  do (rs,ws) <- [format|19
-                   (%u: ("%c"|%u& &( %| ))%n)*
-                   %n
-                   (%s%n)*
-                |]
-
+  do (rs,ws) <- [format|19 (%u: ("%s"|%u& &( %| ))%n)*%n(%s%n)*|]
      let rules1 = IntMap.fromList rs
          rules2 = IntMap.insert  8 (Right [[42   ],[42, 8   ]])
                 $ IntMap.insert 11 (Right [[42,31],[42,11,31]])
                 $ rules1
-
      print (run rules1 ws)
      print (run rules2 ws)
 
@@ -49,9 +43,10 @@ run ::
   IntMap Rule {- ^ parse rules                      -} ->
   [String]    {- ^ input strings                    -} ->
   Int         {- ^ number of matching input strings -}
-run rules ws = count (not . null . R.readP_to_S parser) ws
+run rules ws = count (not . null . readP_to_S parser) ws
   where
-    parser = löb (ruleParser <$> rules) IntMap.! 0 *> R.eof
+    parser = löb (ruleParser <$> rules) IntMap.! 0 *> eof
 
-    ruleParser (Left  s  ) _   = () <$ R.char s
-    ruleParser (Right xss) sub = asum [traverse_ (sub IntMap.!) xs | xs <- xss]
+ruleParser :: Rule -> IntMap (ReadP ()) -> ReadP ()
+ruleParser (Left  s  ) _   = () <$ string s
+ruleParser (Right xss) sub = asum [traverse_ (sub IntMap.!) xs | xs <- xss]
