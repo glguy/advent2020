6c6f233bf515d11a7a6eb3d77468c71913f23a77
Eric Mertens <emertens@gmail.com>
2020-12-24 13:42:04 -0800

    Use enum for day 4

--- execs/Day04.hs
+++ execs/Day04.hs
@@ -1,4 +1,4 @@
-{-# Language BlockArguments, ScopedTypeVariables, QuasiQuotes #-}
+{-# Language BlockArguments, ScopedTypeVariables, QuasiQuotes, TemplateHaskell #-}
 {-|
 Module      : Main
 Description : Day 4 solution
@@ -16,13 +16,14 @@ module Main (main) where
 import Advent
 import Advent.Format (format)
 import Control.Monad
-import Data.Char (isDigit)
+import Data.Char (isDigit, isHexDigit)
 import Data.List (delete, sort)
 import Data.Maybe (isJust)
 import Text.Read (readMaybe)
 
-type Field = (String, String)
-type Passport = [Field]
+type Passport = [(F, String)]
+data F = Fbyr | Fiyr | Feyr | Fhgt | Fhcl | Fecl | Fpid | Fcid deriving (Eq, Ord, Show)
+pure[]
 
 -- |
 -- >>> :main
@@ -30,40 +31,37 @@ type Passport = [Field]
 -- 133
 main :: IO ()
 main =
-  do inp <- [format|4 (%s:%s( |%n))*&%n|]
+  do inp <- [format|4 (@F:%s( |%n))*&%n|]
      print (count complete inp)
      print (count valid inp)
 
-reqFields :: [String]
-reqFields = sort ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+reqFields :: [F]
+reqFields = sort [Fbyr, Fiyr, Feyr, Fhgt, Fhcl, Fecl, Fpid]
 
 complete :: Passport -> Bool
-complete x = reqFields == sort (delete "cid" (map fst x))
+complete x = reqFields == sort (delete Fcid (map fst x))
 
 range :: Ord a => a -> a -> a -> Bool
 range lo hi x = lo <= x && x <= hi
 
-isHex :: Char -> Bool
-isHex x = isDigit x || range 'a' 'f' x
-
 valid :: Passport -> Bool
 valid x = isJust
-  do guard . range (1920::Integer) 2002 =<< readMaybe =<< lookup "byr" x
-     guard . range (2010::Integer) 2020 =<< readMaybe =<< lookup "iyr" x
-     guard . range (2020::Integer) 2030 =<< readMaybe =<< lookup "eyr" x
+  do guard . range (1920::Integer) 2002 =<< readMaybe =<< lookup Fbyr x
+     guard . range (2010::Integer) 2020 =<< readMaybe =<< lookup Fiyr x
+     guard . range (2020::Integer) 2030 =<< readMaybe =<< lookup Feyr x
 
-     (hgtStr, hgtU) <- span isDigit <$> lookup "hgt" x
+     (hgtStr, hgtU) <- span isDigit <$> lookup Fhgt x
      hgt :: Integer <- readMaybe hgtStr
      guard case hgtU of
              "cm" -> range 150 193 hgt
              "in" -> range  59  76 hgt
              _    -> False
 
-     '#':cs <- lookup "hcl" x
-     guard (length cs == 6 && all isHex cs)
+     '#':cs <- lookup Fhcl x
+     guard (length cs == 6 && all isHexDigit cs)
 
-     ecl <- lookup "ecl" x
+     ecl <- lookup Fecl x
      guard (ecl `elem` words "amb blu brn gry grn hzl oth")
 
-     pid <- lookup "pid" x
+     pid <- lookup Fpid x
      guard (length pid == 9 && all isDigit pid)
