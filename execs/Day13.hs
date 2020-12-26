1b0f88ee48c0baba5033c9b47e4a7ba4b5baeb65
Eric Mertens <emertens@gmail.com>
2020-12-26 10:44:33 -0800

    factor out common cabal directives

--- execs/Day13.hs
+++ execs/Day13.hs
@@ -8,14 +8,12 @@ Maintainer  : emertens@gmail.com
 
 <https://adventofcode.com/2020/day/13>
 
-While the general case of part2 can be solved efficiently with
-the Chinese remainder theorem, this local implementation is
-much more straight-forward.
-
 -}
 module Main (main) where
 
+import Advent.Chinese (toMod, chinese)
 import Advent.Format (format)
+import Data.Foldable (traverse_)
 import Data.List (foldl1')
 
 -- |
@@ -27,17 +25,10 @@ main =
   do (t,rawBusses) <- [format|13 %lu%n(x|%lu)&,%n|]
      let busses = [(i,b) | (i, Just b) <- zip [0..] rawBusses]
      print (part1 t (map snd busses))
-     print (part2 busses)
-
-toMod :: Integer -> Integer -> (Integer,Integer)
-toMod x y = (x`mod`y, y)
+     traverse_ print (part2 busses)
 
 part1 :: Integer -> [Integer] -> Integer
-part1 t busses = uncurry (*) (minimum [toMod (-t) b | b <- busses])
+part1 t busses = uncurry (*) (minimum [((-t)`mod`b, b) | b <- busses])
 
-part2 :: [(Integer,Integer)] -> Integer
-part2 busses = fst (foldl1' combine [toMod (-u) b | (u,b) <- busses])
-  where
-    combine (n,m) (a,b)
-      | n `rem` b == a = (n, lcm m b)
-      | otherwise      = combine (n+m, m) (a,b)
+part2 :: [(Integer, Integer)] -> Maybe Integer
+part2 busses = chinese [toMod x y | (x,y) <- busses]
