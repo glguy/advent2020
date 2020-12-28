5e80abf2f06c57ebc0e575ed928bfe0f2ad68c00
Eric Mertens <emertens@gmail.com>
2020-12-27 21:05:05 -0800

    Update Day12.hs

--- execs/Day12.hs
+++ execs/Day12.hs
@@ -15,8 +15,10 @@ import Advent.Coord
 import Advent.Format (format)
 import Data.List (foldl')
 
-type Command = (D, Int)
-data D = DN | DS | DE | DW | DL | DR | DF deriving (Read, Show)
+type Command = Either (D, Int) (T, A)
+data D = DN | DS | DE | DW | DF
+data T = TL | TR
+data A = A90 | A180 | A270
 pure[]
 
 -- | The simulation tracks the current location and the vector used
@@ -37,7 +39,7 @@ mapVect f s = s { vect = f (vect s) }
 -- 41212
 main :: IO ()
 main =
-  do inp <- [format|12 (@D%u%n)*|]
+  do inp <- [format|12 ((@D%u|@T@A)%n)*|]
      print (walk mapHere (Sim origin east                ) inp)
      print (walk mapVect (Sim origin (move 10 east north)) inp)
 
@@ -48,19 +50,17 @@ action ::
   Update Sim Coord {- ^ cardinal direction component -} ->
   Sim -> Command -> Sim
 action mapCard st = \case
-  (DN,   n) -> mapCard (move n north    ) st
-  (DS,   n) -> mapCard (move n south    ) st
-  (DE,   n) -> mapCard (move n east     ) st
-  (DW,   n) -> mapCard (move n west     ) st
-  (DF,   n) -> mapHere (move n (vect st)) st
-  (DL,  90) -> mapVect turnLeft           st
-  (DL, 270) -> mapVect turnRight          st
-  (DL, 180) -> mapVect turnAround         st
-  (DL,   _) -> error "bad left turn"
-  (DR,  90) -> mapVect turnRight          st
-  (DR, 180) -> mapVect turnAround         st
-  (DR, 270) -> mapVect turnLeft           st
-  (DR,   _) -> error "bad right turn"
+  Left  (DN,   n) -> mapCard (move n north    ) st
+  Left  (DS,   n) -> mapCard (move n south    ) st
+  Left  (DE,   n) -> mapCard (move n east     ) st
+  Left  (DW,   n) -> mapCard (move n west     ) st
+  Left  (DF,   n) -> mapHere (move n (vect st)) st
+  Right (TL, A90) -> mapVect turnLeft           st
+  Right (TL,A270) -> mapVect turnRight          st
+  Right (TL,A180) -> mapVect turnAround         st
+  Right (TR, A90) -> mapVect turnRight          st
+  Right (TR,A180) -> mapVect turnAround         st
+  Right (TR,A270) -> mapVect turnLeft           st
 
 move :: Int -> Coord -> Coord -> Coord
 move n v = addCoord (scaleCoord n v)
